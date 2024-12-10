308f554d290dfc9666eda01cf3c02f39870b6027
arianelsrh <a.rosvold-haghpanah0420221@arts.ac.uk>
2024-12-10 03:13:54 -1200

    vsco-friendly-version

--- sketch.js
+++ sketch.js
@@ -1,7 +1,47 @@
+let balls = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
+  noStroke();
 }
 
 function draw() {
-  background(220);
+  background(30);
+  for (let ball of balls) {
+    ball.move();
+    ball.display();
+  }
+}
+
+function mousePressed() {
+  balls.push(new Ball(mouseX, mouseY));
+}
+
+class Ball {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = random(20, 50);
+    this.xSpeed = random(-3, 3);
+    this.ySpeed = random(-3, 3);
+    this.color = color(random(100, 255), random(100, 255), random(100, 255));
+  }
+
+  move() {
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+
+    // Bounce off walls
+    if (this.x < 0 || this.x > width) {
+      this.xSpeed *= -1;
+    }
+    if (this.y < 0 || this.y > height) {
+      this.ySpeed *= -1;
+    }
+  }
+
+  display() {
+    fill(this.color);
+    ellipse(this.x, this.y, this.size);
+  }
 }
